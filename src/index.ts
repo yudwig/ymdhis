abfd5bc4bd116f8e525d330eab56e09627877237
yudwig <yudwig.dev@gmail.com>
2022-09-13 21:04:01 +0900

    Format codes.

--- src/index.ts
+++ src/index.ts
@@ -1,198 +1,255 @@
 interface Props {
-    date: Date
-    options?: Options
+  date: Date;
+  options?: Options;
 }
 
 interface Options {
+  // Separators
+  yearSeparator: string;
+  monthSeparator: string;
+  daySeparator: string;
+  dateTimeSeparator: string;
+  hourSeparator: string;
+  minuteSeparator: string;
+  secondSeparator: string;
+  dowSeparators: [string, string];
 
-    // Separators
-    yearSeparator: string;
-    monthSeparator: string;
-    daySeparator: string;
-    dateTimeSeparator: string;
-    hourSeparator: string;
-    minuteSeparator: string;
-    secondSeparator: string;
-    dowSeparators: [string, string];
+  // Notations
+  amNotation: string;
+  pmNotation: string;
+  monthNotations: string[];
+  dateNotations: string[];
+  dowNotations: string[];
 
-    // Notations
-    amNotation: string;
-    pmNotation: string;
-    monthNotations: string[];
-    dateNotations: string[];
-    dowNotations: string[];
-
-    // Digits
-    isYearAsFourDigits: boolean;
-    isEnablePaddingYear: boolean;
-    isMonthAsTwoDigits: boolean;
-    isDayAsTwoDigits: boolean;
-    isHourAsTwoDigits: boolean;
-    isMinuteAsTwoDigits: boolean;
-    isSecondAsTwoDigits: boolean;
+  // Digits
+  isYearAsFourDigits: boolean;
+  isEnablePaddingYear: boolean;
+  isMonthAsTwoDigits: boolean;
+  isDayAsTwoDigits: boolean;
+  isHourAsTwoDigits: boolean;
+  isMinuteAsTwoDigits: boolean;
+  isSecondAsTwoDigits: boolean;
 }
 
 class Ymdhis {
+  readonly date: Date;
 
-    readonly date: Date;
+  private options: Options = {
+    // Separators
+    yearSeparator: "-",
+    monthSeparator: "-",
+    daySeparator: "-",
+    dateTimeSeparator: " ",
+    hourSeparator: ":",
+    minuteSeparator: ":",
+    secondSeparator: ":",
+    dowSeparators: [" (", ")"],
 
-    private options: Options = {
+    // Notations
+    amNotation: "AM",
+    pmNotation: "PM",
+    monthNotations: [],
+    dateNotations: [],
+    dowNotations: [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ],
 
-        // Separators
-        yearSeparator: "-",
-        monthSeparator: "-",
-        daySeparator: "-",
-        dateTimeSeparator: " ",
-        hourSeparator: ":",
-        minuteSeparator: ":",
-        secondSeparator: ":",
-        dowSeparators: [" (", ")"],
+    // Digits
+    isYearAsFourDigits: true,
+    isEnablePaddingYear: true,
+    isMonthAsTwoDigits: true,
+    isDayAsTwoDigits: true,
+    isHourAsTwoDigits: true,
+    isMinuteAsTwoDigits: true,
+    isSecondAsTwoDigits: true,
+  };
 
-        // Notations
-        amNotation: "AM",
-        pmNotation: "PM",
-        monthNotations: [],
-        dateNotations: [],
-        dowNotations: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-
-        // Digits
-        isYearAsFourDigits: true,
-        isEnablePaddingYear: true,
-        isMonthAsTwoDigits: true,
-        isDayAsTwoDigits: true,
-        isHourAsTwoDigits: true,
-        isMinuteAsTwoDigits: true,
-        isSecondAsTwoDigits: true,
+  constructor(props: Props) {
+    this.date = new Date(props.date.getTime());
+    if (typeof props.options !== "undefined") {
+      this.options = Object.assign({}, props.options);
     }
+  }
 
-    constructor(props: Props) {
-        this.date = new Date(props.date.getTime());
-        if (typeof props.options !== "undefined") {
-            this.options = Object.assign({}, props.options);
-        }
-    }
+  get year(): number {
+    return this.date.getFullYear();
+  }
 
-    get year(): number {
-        return this.date.getFullYear();
-    }
+  get month(): number {
+    return this.date.getMonth() + 1;
+  }
 
-    get month(): number {
-        return this.date.getMonth() + 1;
-    }
+  get day(): number {
+    return this.date.getDate();
+  }
 
-    get day(): number {
-        return this.date.getDate();
-    }
+  get dow() {
+    return this.date.getDay();
+  }
 
-    get dow() {
-        return this.date.getDay();
-    }
+  get hour() {
+    return this.date.getHours();
+  }
 
-    get hour() {
-        return this.date.getHours();
-    }
+  get minute() {
+    return this.date.getMinutes();
+  }
 
-    get minute() {
-        return this.date.getMinutes();
-    }
+  get second() {
+    return this.date.getSeconds();
+  }
 
-    get second() {
-        return this.date.getSeconds();
-    }
+  get ampm() {
+    return this.date.getHours() % 12;
+  }
 
-    get ampm() {
-        return this.date.getHours() % 12;
-    }
+  get y() {
+    return;
+  }
+  get m() {
+    return;
+  }
+  get d() {
+    return;
+  }
+  get h() {
+    return;
+  }
+  get i() {
+    return;
+  }
+  get s() {
+    return;
+  }
+  get w() {
+    return;
+  }
+  get a() {
+    return;
+  }
+  get ymd() {
+    return;
+  }
+  get ymdhi() {
+    return;
+  }
+  get ymdw() {
+    return;
+  }
+  get wymd() {
+    return;
+  }
+  get dmy() {
+    return;
+  }
+  get dm() {
+    return;
+  }
+  get mdy() {
+    return;
+  }
+  get md() {
+    return;
+  }
+  get hi() {
+    return;
+  }
+  get his() {
+    return;
+  }
+  get hia() {
+    return;
+  }
+  get hisa() {
+    return;
+  }
+  get ahi() {
+    return;
+  }
+  get ahis() {
+    return;
+  }
 
-    get y() { return; }
-    get m() { return; }
-    get d() { return; }
-    get h() { return; }
-    get i() { return; }
-    get s() { return; }
-    get w() { return; }
-    get a() { return; }
-    get ymd() { return; }
-    get ymdhi() { return; }
-    get ymdw() { return; }
-    get wymd() { return; }
-    get dmy() { return; }
-    get dm() { return; }
-    get mdy() { return; }
-    get md() { return; }
-    get hi() { return; }
-    get his() { return; }
-    get hia() { return; }
-    get hisa() { return; }
-    get ahi() { return; }
-    get ahis() { return; }
+  get number() {
+    return (
+      this.date.getFullYear() * 10000000000 +
+      (this.date.getMonth() + 1) * 100000000 +
+      this.date.getDate() * 1000000 +
+      this.date.getHours() * 10000 +
+      this.date.getMinutes() * 100 +
+      this.date.getSeconds()
+    );
+  }
 
-    get number() {
-        return this.date.getFullYear() * 10000000000 +
-          (this.date.getMonth() + 1) * 100000000 +
-          this.date.getDate() * 1000000 +
-          this.date.getHours() * 10000 +
-          this.date.getMinutes() * 100 +
-          this.date.getSeconds();
-    }
+  get iso9075() {
+    return;
+  }
+  get iso8601() {
+    return;
+  }
+  get rfc3339() {
+    return;
+  }
 
-    get iso9075() { return; }
-    get iso8601() { return; }
-    get rfc3339() { return; }
+  afterYears() {}
+  afterMonths() {}
+  afterWeeks() {}
+  afterDays() {}
+  afterHours() {}
+  afterMinutes() {}
+  afterSeconds() {}
+  beforeYears() {}
+  beforeMonths() {}
+  beforeWeeks() {}
+  beforeDays() {}
+  beforeHours() {}
+  beforeMinutes() {}
+  beforeSeconds() {}
+  separateDateBy() {}
+  separateTimeBy() {}
+  separateDateTimeBy() {}
+  separateItemsBy() {}
+  encloseDowIn() {}
+  noPaddingYear() {}
+  noPaddingDate() {}
+  noPaddingMonth() {}
+  noPaddingDay() {}
+  noPaddingTime() {}
+  noPaddingHours() {}
+  noPaddingMinutes() {}
+  noPaddingSeconds() {}
+  ampmAs() {}
 
-    afterYears() {}
-    afterMonths() {}
-    afterWeeks() {}
-    afterDays() {}
-    afterHours() {}
-    afterMinutes() {}
-    afterSeconds() {}
-    beforeYears() {}
-    beforeMonths() {}
-    beforeWeeks() {}
-    beforeDays() {}
-    beforeHours() {}
-    beforeMinutes() {}
-    beforeSeconds() {}
-    separateDateBy() {}
-    separateTimeBy() {}
-    separateDateTimeBy() {}
-    separateItemsBy() {}
-    encloseDowIn() {}
-    noPaddingYear() {}
-    noPaddingDate() {}
-    noPaddingMonth() {}
-    noPaddingDay() {}
-    noPaddingTime() {}
-    noPaddingHours() {}
-    noPaddingMinutes() {}
-    noPaddingSeconds() {}
-    ampmAs() {}
+  dowAs(dowList: string[]): Ymdhis {
+    return this.cloneWithOptions({ dowNotations: dowList });
+  }
 
-    dowAs(dowList: string[]): Ymdhis {
-        return this.cloneWithOptions({dowNotations: dowList})
-    }
+  monthsAs() {}
+  datesAs() {}
+  yearAsTwoDigits() {}
+  toString() {}
 
-    monthsAs() {}
-    datesAs() {}
-    yearAsTwoDigits() {}
-    toString() {}
+  initDate(date: Date) {
+    return new Ymdhis({
+      date: date,
+      options: this.options,
+    });
+  }
 
-    initDate(date: Date) {
-        return new Ymdhis({
-            date: date,
-            options: this.options
-        })
-    }
-
-    private cloneWithOptions(options: Partial<Options>): Ymdhis {
-        return new Ymdhis({
-            date: this.date,
-            options: Object.assign(this.options, options)
-        })
-    }
+  private cloneWithOptions(options: Partial<Options>): Ymdhis {
+    return new Ymdhis({
+      date: this.date,
+      options: Object.assign(this.options, options),
+    });
+  }
 }
 
 export function ymdhis(date: Date = new Date()): Ymdhis {
-    return new Ymdhis({date: date});
+  return new Ymdhis({ date: date });
 }
--- src/test.ts
+++ src/test.ts
@@ -1,45 +1,42 @@
-import { ymdhis } from "./index"
-
+import { ymdhis } from "./index";
 
 describe("Basic alias properties", () => {
   // 2000-01-02 12:34:56 (Sunday)
   const date = ymdhis(new Date(2000, 0, 2, 12, 34, 56));
   it("year", () => {
-    expect(date.year).toBe(2000)
-  })
+    expect(date.year).toBe(2000);
+  });
   it("month", () => {
-    expect(date.month).toBe(1)
-  })
+    expect(date.month).toBe(1);
+  });
   it("day", () => {
-    expect(date.day).toBe(2)
-  })
+    expect(date.day).toBe(2);
+  });
   it("dow", () => {
-    expect(date.dow).toBe(0)
-  })
+    expect(date.dow).toBe(0);
+  });
   it("hour", () => {
-    expect(date.hour).toBe(12)
-  })
+    expect(date.hour).toBe(12);
+  });
   it("minute", () => {
-    expect(date.minute).toBe(34)
-  })
+    expect(date.minute).toBe(34);
+  });
   it("second", () => {
-    expect(date.second).toBe(56)
-  })
-})
+    expect(date.second).toBe(56);
+  });
+});
 
 describe("Calculated properties", () => {
   it("ampm", () => {
-    expect(ymdhis(new Date(2000, 0, 1, 0, 0, 0)).ampm).toBe(0)
-    expect(ymdhis(new Date(2000, 0, 1, 1, 0, 0)).ampm).toBe(1)
-    expect(ymdhis(new Date(2000, 0, 1, 11, 0, 0)).ampm).toBe(11)
-    expect(ymdhis(new Date(2000, 0, 1, 12, 0, 0)).ampm).toBe(0)
-    expect(ymdhis(new Date(2000, 0, 1, 23, 0, 0)).ampm).toBe(11)
-  })
+    expect(ymdhis(new Date(2000, 0, 1, 0, 0, 0)).ampm).toBe(0);
+    expect(ymdhis(new Date(2000, 0, 1, 1, 0, 0)).ampm).toBe(1);
+    expect(ymdhis(new Date(2000, 0, 1, 11, 0, 0)).ampm).toBe(11);
+    expect(ymdhis(new Date(2000, 0, 1, 12, 0, 0)).ampm).toBe(0);
+    expect(ymdhis(new Date(2000, 0, 1, 23, 0, 0)).ampm).toBe(11);
+  });
   it("number", () => {
     expect(ymdhis(new Date(2022, 0, 2, 12, 34, 56)).number).toBe(
       20220102123456
-    )
-  })
-})
-
-
+    );
+  });
+});
