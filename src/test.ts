032319e951dc4c237a46da95939da8e1ecfc186b
yudwig <yudwig.dev@gmail.com>
2022-09-14 09:37:10 +0900

    Add tests.

--- src/test.ts
+++ src/test.ts
@@ -40,3 +40,82 @@ describe("Calculated properties", () => {
     );
   });
 });
+
+describe("Format date functions", () => {
+  it("y: digits 4", () => {
+    expect(ymdhis(new Date(1987, 0, 1)).y).toBe("1987");
+  });
+  it("y: digits 2, enable padding", () => {
+    expect(ymdhis(new Date(1987, 0, 1)).yearAsTwoDigits().y).toBe("87");
+    expect(ymdhis(new Date(2003, 0, 1)).yearAsTwoDigits().y).toBe("03");
+    expect(ymdhis(new Date(2000, 0, 1)).yearAsTwoDigits().y).toBe("00");
+  });
+  it("y: digits 2, disable padding", () => {
+    expect(
+      ymdhis(new Date(1987, 0, 1)).yearAsTwoDigits().noPaddingYear().y
+    ).toBe("87");
+    expect(
+      ymdhis(new Date(2003, 0, 1)).yearAsTwoDigits().noPaddingYear().y
+    ).toBe("3");
+    expect(
+      ymdhis(new Date(2000, 0, 1)).yearAsTwoDigits().noPaddingYear().y
+    ).toBe("0");
+  });
+  it("m: enable padding", () => {
+    expect(ymdhis(new Date(2000, 0, 1)).m).toBe("01");
+    expect(ymdhis(new Date(2000, 11, 1)).m).toBe("12");
+  });
+  it("m: disable padding", () => {
+    expect(ymdhis(new Date(2000, 0, 1)).noPaddingMonth().m).toBe("1");
+    expect(ymdhis(new Date(2000, 11, 1)).noPaddingMonth().m).toBe("12");
+  });
+  it("d: enable padding", () => {
+    expect(ymdhis(new Date(2000, 0, 1)).d).toBe("01");
+    expect(ymdhis(new Date(2000, 0, 20)).d).toBe("20");
+  });
+  it("d: disable padding", () => {
+    expect(ymdhis(new Date(2000, 0, 1)).noPaddingDay().d).toBe("1");
+    expect(ymdhis(new Date(2000, 0, 20)).noPaddingDay().d).toBe("20");
+  });
+  it("noPaddingDate()", () => {
+    expect(ymdhis(new Date(2000, 0, 1)).noPaddingDate().y).toBe("2000");
+    expect(ymdhis(new Date(2000, 0, 1)).noPaddingDate().m).toBe("1");
+    expect(ymdhis(new Date(2000, 0, 1)).noPaddingDate().d).toBe("1");
+    expect(ymdhis(new Date(2000, 11, 20)).noPaddingDate().m).toBe("12");
+    expect(ymdhis(new Date(2000, 11, 20)).noPaddingDate().d).toBe("20");
+  });
+  it("h: enable padding", () => {
+    expect(ymdhis(new Date(2000, 0, 1, 1, 0, 0)).h).toBe("01");
+    expect(ymdhis(new Date(2000, 0, 1, 12, 0, 0)).h).toBe("12");
+  });
+  it("h: disable padding", () => {
+    expect(ymdhis(new Date(2000, 0, 1, 1, 0, 0)).noPaddingHours().h).toBe("1");
+    expect(ymdhis(new Date(2000, 0, 1, 12, 0, 0)).noPaddingHours().h).toBe(
+      "12"
+    );
+  });
+  it("i: enable padding", () => {
+    expect(ymdhis(new Date(2000, 0, 1, 1, 1, 0)).i).toBe("01");
+    expect(ymdhis(new Date(2000, 0, 1, 1, 20, 0)).i).toBe("20");
+  });
+  it("i: disable padding", () => {
+    expect(ymdhis(new Date(2000, 0, 1, 1, 1, 0)).noPaddingMinutes().i).toBe(
+      "1"
+    );
+    expect(ymdhis(new Date(2000, 0, 1, 1, 20, 0)).noPaddingMinutes().i).toBe(
+      "20"
+    );
+  });
+  it("s: enable padding", () => {
+    expect(ymdhis(new Date(2000, 0, 1, 1, 1, 1)).s).toBe("01");
+    expect(ymdhis(new Date(2000, 0, 1, 1, 1, 20)).s).toBe("20");
+  });
+  it("s: disable padding", () => {
+    expect(ymdhis(new Date(2000, 0, 1, 1, 1, 1)).noPaddingSeconds().s).toBe(
+      "1"
+    );
+    expect(ymdhis(new Date(2000, 0, 1, 1, 1, 20)).noPaddingSeconds().s).toBe(
+      "20"
+    );
+  });
+});
